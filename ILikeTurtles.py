863cd1149e29f8a858c9d1955d7b3fc553bff953
gany <gany@gilmour.org>
2018-09-17 15:11:33 -0400

    Forward and stop

--- ILikeTurtles.py
+++ ILikeTurtles.py
@@ -1,11 +1,5 @@
-from turtle import Turtle
-
-t=Turtle()
-t.shape("turtle")
-
-
-for x in range(50):
-    t.forward(50)
-    t.right(50)
-
-input("Press any key to exit")
+from gopigo import *
+import time
+fwd()
+time.sleep(5)
+stop()
